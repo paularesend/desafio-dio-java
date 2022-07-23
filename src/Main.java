f6fb0861d1f6d8ae92b5ee49c3139643e7f2bd33
Paula Resende <paula-resende@live.com>
2022-07-23 00:46:32 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import dominio.Curso;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+		
+		curso1.setTitulo("curso java");
+
+		curso1.setDescricao("descrição curso java");
+		System.out.println(curso1);
+
+	}
+
+}
